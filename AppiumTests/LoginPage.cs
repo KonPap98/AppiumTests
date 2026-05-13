5ab4b304df5370a313d0f3b9a884524315ca4278
KonPap98 <159794640+KonPap98@users.noreply.github.com>
2026-05-13 20:46:54 +0300

    Add project files.

---
+++ AppiumTests/LoginPage.cs
@@ -0,0 +1,26 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Appium.Android;
+
+namespace AppiumTests
+{
+    public class LoginPage
+    {
+        private readonly AndroidDriver _driver;
+
+        public LoginPage(AndroidDriver driver)
+        {
+            _driver = driver;
+        }
+
+        private By UsernameInput => By.Id("com.example.app:id/username");
+        private By PasswordInput => By.Id("com.example.app:id/password");
+        private By LoginButton => By.Id("com.example.app:id/login_button");
+
+        public void Login(string username, string password)
+        {
+            _driver.FindElement(UsernameInput).SendKeys(username);
+            _driver.FindElement(PasswordInput).SendKeys(password);
+            _driver.FindElement(LoginButton).Click();
+        }
+    }
+}
---
+++ AppiumTests/Program.cs
@@ -0,0 +1,25 @@
+﻿using OpenQA.Selenium.Appium;
+using OpenQA.Selenium.Appium.Android;
+
+var options = new AppiumOptions();
+
+options.PlatformName = "Android";
+options.AutomationName = "UiAutomator2";
+
+// Your emulator from adb devices
+options.AddAdditionalAppiumOption("udid", "emulator-5554");
+
+// Optional
+options.DeviceName = "Android Emulator";
+
+options.AddAdditionalAppiumOption("appPackage", "com.EspanolDictionary");
+options.AddAdditionalAppiumOption("appActivity", "com.EspanolDictionary.MainActivity");
+
+var driver = new AndroidDriver(
+    new Uri("http://127.0.0.1:4723"),
+    options
+);
+
+Console.WriteLine("Connected to Appium successfully!");
+
+driver.Quit();
