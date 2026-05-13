dbfb90e50773b4021e59e171727ef9331b541c56
KonPap98 <159794640+KonPap98@users.noreply.github.com>
2026-05-14 00:43:48 +0300

    Folder structure

---
+++ AppiumTests/Base/AppiumTestBase.cs
@@ -0,0 +1,37 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium.Appium;
+using OpenQA.Selenium.Appium.Android;
+
+namespace AppiumTests.Base
+{
+    public class AppiumTestBase
+    {
+        protected AndroidDriver Driver;
+
+        [SetUp]
+        public void SetUp()
+        {
+            var options = new AppiumOptions();
+
+            options.PlatformName = "Android";
+            options.AutomationName = "UiAutomator2";
+            options.DeviceName = "Android Emulator";
+
+            options.AddAdditionalAppiumOption("udid", "emulator-5554");
+            options.AddAdditionalAppiumOption("appPackage", "com.EspanolDictionary");
+            options.AddAdditionalAppiumOption("noReset", true);
+
+            Driver = new AndroidDriver(
+                new Uri("http://127.0.0.1:4723"),
+                options
+            );
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            Driver?.Quit();
+        }
+    }
+}
+
---
+++ AppiumTests/Pages/LoginPage.cs
@@ -0,0 +1,34 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Appium;
+using OpenQA.Selenium.Appium.Android;
+
+namespace AppiumTests.Pages
+{
+    public class LoginPage
+    {
+        private readonly AndroidDriver _driver;
+
+        public LoginPage(AndroidDriver driver)
+        {
+            _driver = driver;
+        }
+
+
+        private By EmailInput => By.XPath("//android.widget.EditText[@hint='Email']");
+        private By PasswordInput => By.XPath("//android.widget.EditText[@hint='Password']");
+        private By LoginButton => MobileBy.AccessibilityId("LOGIN");
+
+        public void Login(string email, string password)
+        { 
+            var emailField = _driver.FindElement(EmailInput);
+            emailField.Click();
+            emailField.SendKeys(email);
+
+            var passwordField = _driver.FindElement(PasswordInput);
+            passwordField.Click();
+            passwordField.SendKeys(password);
+
+            _driver.FindElement(LoginButton).Click();
+        }
+    }
+}
